1e9acace27593876a0430ae615763fd2652ecf59
Juraj Brabec <juraj@brabec.sk>
2024-05-08 15:12:57 +0200

    modifies init script

--- mariadb/mysql-init/90-init-db.sh
+++ mariadb/mysql-init/90-init-db.sh
@@ -2,7 +2,7 @@ init_db() {
   local thisdir
   thisdir=$(dirname ${BASH_SOURCE[0]})
   log_info "Initializing the database..."
-  mysql $mysql_flags ${MYSQL_DATABASE} < $(cat $thisdir/init_db.sql)
+  mysql $mysql_flags ${MYSQL_DATABASE} < $thisdir/init_db.sql
 }
 
 if ! [ -v MYSQL_RUNNING_AS_SLAVE ] && $MYSQL_DATADIR_FIRST_INIT ; then
